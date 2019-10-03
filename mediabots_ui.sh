3403dc1b2b07da0688ba43a7c78973d21f28567f
best7766 <48321167+best7766@users.noreply.github.com>
2019-10-03 19:23:26 +0300

    Update mediabots_ui.sh

--- mediabots_ui.sh
+++ mediabots_ui.sh
@@ -24,7 +24,6 @@ if [ ! -z $wordpress_installation ] && [ $wordpress_installation = 'Y' -o $wordp
 	echo ".:Advanced Setting:. It is Optional"
 	wpdb_choice=$(echo "Y" | head -c 1)
 fi
-
 if [ -z $domain ] && [ $wordpress_installation_ -eq 1 ]; then
 	domain='wordpress'
 	host=0
